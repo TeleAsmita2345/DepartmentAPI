8218ba1a72c19033da7ca2ac490e3395b9091fcc
asmita.tele <asmita.tele@AITLP-307>
2022-07-01 19:12:12 +0530

    Add project files.

---
+++ DepartmentAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,85 @@
+﻿using DepartmentAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDepartmentData()
+        {
+            using (var dbContext=new DepartmentDBContext())
+            {
+                return Ok(dbContext.Departments.ToList());
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDepartmentById(int id)
+        {
+            using (var dbContext=new DepartmentDBContext())
+            {
+                var department = dbContext.Departments.Where(x=> x.Deptid==id).FirstOrDefault();
+                return Ok(department);
+                
+            }
+        }
+
+        [HttpPost]
+        public void InsertDeparment([FromBody]DepartmentData data)
+        {
+            using(var dbContext=new DepartmentDBContext())
+            {
+                dbContext.Departments.Add(new Department()
+                {
+                    Deptid = data.Id,
+                    Deptname = data.Dname
+
+                });
+
+                dbContext.SaveChanges();  
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateDepartment(DepartmentData data)
+        {
+            using (var dbContext = new DepartmentDBContext())
+            {
+               Department dept = dbContext.Departments.Where(x => x.Deptid == data.Id).FirstOrDefault();
+
+                if (dept!= null)
+                {
+                    dept.Deptid = data.Id;
+                    dept.Deptname = data.Dname;
+                }
+                else
+                {
+                    return BadRequest(StatusCodes.Status400BadRequest);
+
+                }
+
+                dbContext.Departments.Update(dept);
+                dbContext.SaveChanges();
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDepartment(int id)
+        {
+            using(var dbContext=new DepartmentDBContext())
+            {
+               var data= dbContext.Departments.Where(x => x.Deptid == id).FirstOrDefault();
+                dbContext.Departments.Remove(data);
+                dbContext.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ DepartmentAPI/Models/DepartmentData.cs
@@ -0,0 +1,8 @@
+﻿namespace DepartmentAPI.Models
+{
+    public class DepartmentData
+    {
+        public int Id { get; set; }
+        public string Dname { get; set; }
+    }
+}
